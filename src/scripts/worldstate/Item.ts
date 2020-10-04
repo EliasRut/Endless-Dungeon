f0e8cc9dac764141bd1db82e059ad37fb776581f
Franz Leberl <franz_leberl@hotmail.com>
2020-10-04 22:05:15 +0200

    item fixes

--- src/scripts/worldstate/Item.ts
+++ src/scripts/worldstate/Item.ts
@@ -1,15 +1,26 @@
 // This class handles the items.
 import PlayerCharacter from '../worldstate/PlayerCharacter';
-export default class Item extends Phaser.Physics.Arcade.Sprite{
+export default class Item {
     public itemHealth = 10;
     public weaponDamage = 1;
-    public itemMovementspeed = 0;
+    public itemMovementSpeed = 0;
     public mainStat = 1;
 
+    constructor(
+        itemHealth: number,
+        weaponDamage: number,
+        itemMovementSpeed: number,
+        mainStat: number
+      ) {
+      this.itemHealth = itemHealth;
+      this.weaponDamage = weaponDamage;
+      this.itemMovementSpeed = itemMovementSpeed;
+      this.mainStat = mainStat;
+    }
     public equip(player: PlayerCharacter){
         player.itemHealth = this.itemHealth;
         player.weaponDamage = this.weaponDamage;
-        player.itemMovementSpeed = this.itemMovementspeed;
+        player.itemMovementSpeed = this.itemMovementSpeed;
         player.mainStat = this.mainStat;
         player.updateStats();
     }
