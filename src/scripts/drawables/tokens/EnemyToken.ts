a26680696659d085d2538bb31cc3619a4f02ec7d
EllaHoefl <raphaela.hoefl@gmx.at>
2022-09-25 19:52:37 +0200

    Fixed bug in attack pattern for enemy type pierre.

--- src/scripts/drawables/tokens/EnemyToken.ts
+++ src/scripts/drawables/tokens/EnemyToken.ts
@@ -419,7 +419,7 @@ export default class EnemyToken extends CharacterToken {
 	}
 
 	executeRangedAttack(time: number) {
-		if (!this.isWaitingToAttack) {
+		if (!this.isWaitingToAttack && !this.isCasting) {
 			this.attackedAt = time;
 			this.isWaitingToAttack = true;
 			this.setVelocityX(0);
